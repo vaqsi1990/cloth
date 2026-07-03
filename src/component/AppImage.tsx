e76afd7e54ca06e42c694c9a32487ffba18d9073
vaqsi1990 <vaqsii23@gmail.com>
2026-07-03 20:59:27 +0400

    image

--- src/component/AppImage.tsx
+++ src/component/AppImage.tsx
@@ -1,6 +1,8 @@
 import {
   type ComponentPropsWithoutRef,
   type CSSProperties,
+  type DragEvent,
+  type MouseEvent,
 } from 'react'
 
 export type ImageProps = Omit<
@@ -19,10 +21,16 @@ export type ImageProps = Omit<
   blurDataURL?: string
   loader?: unknown
   onLoadingComplete?: unknown
+  /** When false, allows normal save/drag. Default true. */
+  protectFromDownload?: boolean
 }
 
 function joinClassNames(...classes: Array<string | undefined | false>) {
   return classes.filter(Boolean).join(' ')
+}
+
+function preventImageDownload(event: MouseEvent | DragEvent) {
+  event.preventDefault()
 }
 
 export default function Image({
@@ -41,13 +49,27 @@ export default function Image({
   blurDataURL: _blurDataURL,
   loader: _loader,
   onLoadingComplete: _onLoadingComplete,
+  protectFromDownload = true,
   loading,
   fetchPriority,
+  onContextMenu,
+  onDragStart,
+  draggable,
   ...rest
 }: ImageProps) {
   const imgClassName = fill
     ? joinClassNames('absolute inset-0 h-full w-full', className)
     : className
+
+  const protectedStyle: CSSProperties | undefined = protectFromDownload
+    ? {
+        ...(style as CSSProperties | undefined),
+        userSelect: 'none',
+        WebkitUserSelect: 'none',
+        WebkitUserDrag: 'none',
+        WebkitTouchCallout: 'none',
+      }
+    : (style as CSSProperties | undefined)
 
   return (
     <img
@@ -57,9 +79,26 @@ export default function Image({
       width={fill ? undefined : width}
       height={fill ? undefined : height}
       className={imgClassName}
-      style={style as CSSProperties | undefined}
+      style={protectedStyle}
       loading={priority ? 'eager' : loading}
       fetchPriority={priority ? 'high' : fetchPriority}
+      draggable={protectFromDownload ? false : draggable}
+      onContextMenu={
+        protectFromDownload
+          ? (event) => {
+              preventImageDownload(event)
+              onContextMenu?.(event)
+            }
+          : onContextMenu
+      }
+      onDragStart={
+        protectFromDownload
+          ? (event) => {
+              preventImageDownload(event)
+              onDragStart?.(event)
+            }
+          : onDragStart
+      }
     />
   )
 }
